652602c42c1ebbfbc0d8068f60925a1a7a5ee596
pankajtirkey <pankaj.tirkey@mail.vinove.com>
2019-10-16 14:30:07 +0530

    Completed section 10

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,16 +1,16 @@
-/* 
+/*
 setTimeout(() => {
   console.log('Two seconds are up.');
 }, 2000);
  */
 
-/* 
+/*
 const names = ['Pankaj', 'John', 'Lijo'];
 const shortNames = names.filter(name => name.length < 6);
 console.log(shortNames);
  */
 
-/*  
+/*
 const geoCode = (adrress, cb) => {
   setTimeout(() => {
     const data = {
@@ -26,13 +26,27 @@ geoCode('Lodhi Colony', data => {
 });
  */
 
-const add = (a, b, cb) => {
+/* const add = (a, b, cb) => {
   setTimeout(() => {
     const sum = a + b;
     cb(sum);
   }, 2000);
-};
+}; */
 
-add(1, 4, sum => {
+/* add(1, 4, sum => {
   console.log('The sum is ', sum);
-});
+}); */
+
+const doWorkCallback = (callback) => {
+  setTimeout(() => {
+    callback(/* 'This is my error', undefined */undefined, [1, 4, 3]);
+  }, 1500)
+}
+
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(result);
+
+})
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,7 +2,6 @@ const mongodb = require('mongodb');
 
 const { MongoClient, ObjectID } = mongodb;
 
-
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager'
 
@@ -20,6 +19,68 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     console.log(user);
   }) */
 
-  db.collection('users').find({ age: 25 }, () => { })
+  /* db.collection('users').find({ age: 25 }).toArray((error, user) => {
+    if (error) {
+      return console.log('Unable to find!');
+    }
+    console.log(user);
+  }) */
+
+  /* db.collection('users').find({ age: { $gte: 25 } }).count((error, count) => {
+    if (error) {
+      return console.log('Unable to find!');
+    }
+    console.log(count);
+  }) */
+
+  /* db.collection('tasks').findOne({ _id: ObjectID("5da6a77a2a6f6e8f3a8845c1") }, (error, task) => {
+    if (error) {
+      return console.log('Unable to fetch task!');
+    }
+    console.log(task);
+  }) */
+
+  /* db.collection('tasks').find({ completed: false }).toArray((error, task) => {
+    if (error) {
+      console.log('Unable to find!');
+    }
+    console.log(task);
+  }) */
+
+  /* db.collection('users').updateOne({ _id: ObjectID("5da5a32feebf461ad8eb09dd") }, { $inc: { age: 1 } }).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  }) */
+
+  /* db.collection('tasks').updateMany({ completed: false }, { $set: { completed: true } }).then(result => {
+    console.log(result.modifiedCount);
+  }).catch(error => {
+    console.log(error);
+  }) */
+
+  /* db.collection('users').deleteOne({ age: { $gt: 25 } }).then(result => {
+    console.log(result.deletedCount);
+  }).catch(error => {
+    console.log(error);
+  }) */
+
+  /* db.collection('users').deleteMany({ age: { $gt: 18 } }).then(result => {
+    console.log(result.deletedCount);
+  }).catch(error => {
+    console.log(error);
+  }) */
+
+  /* db.collection('tasks').deleteOne({ name: "Setup editor" }).then(result => {
+    console.log(result.deletedCount)
+  }).catch(error => {
+    console.log(error);
+  }); */
+
+  /* db.collection('tasks').deleteMany({ completed: true }).then(result => {
+    console.log(result.deletedCount);
+  }).catch(error => {
+    console.log(error);
+  }) */
 
 })
