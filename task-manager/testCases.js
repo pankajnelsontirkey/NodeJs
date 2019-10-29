c62237d6373e4bb0cc07e9b351202ba2b1629352
pankajtirkey <pankaj.tirkey@mail.vinove.com>
2019-10-29 10:20:52 +0530

    Finished Section 16, added addtional test cases file.

---
+++ task-manager/testCases.js
@@ -0,0 +1,10 @@
+/**
+ * USER TEST CASES
+ */
+
+//  Should not signup with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
+
+/* TASK TEST CASES */
