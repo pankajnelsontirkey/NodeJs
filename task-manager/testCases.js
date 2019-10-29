7182908c4d23963cf63674538e53b987a52491a0
pankajtirkey <pankaj.tirkey@mail.vinove.com>
2019-10-29 10:45:26 +0530

    Finished Section 16, added addtional test cases file.

--- task-manager/testCases.js
+++ task-manager/testCases.js
@@ -8,3 +8,16 @@
 // Should not delete user if unauthenticated
 
 /* TASK TEST CASES */
+
+// Should not creta task with invalid description/completed
+// Should not update task with invalid description/completed
+// Should delete user task
+// Should not delete task if unauthenticated
+// Should not update other users task
+// Should fetch user task by id
+// Should not fetch user task by id if unauthenticated
+// Should not fetch other users tasks by id
+// Should fetch only completed tasks
+// Should fetch only completed tasks
+// Should sort tasks by description/completed/createdAt/updatedAt
+// Should fetch page of tasks
